a220195d75ffe3753d624d3d87eb8014b3e36bee
Aleksi Martikainen <aleksi.martikainen@devecto.com>
2022-04-02 12:01:15 +0300

    Add player movement

--- ldjam50-lohja/Assets/Scripts/PlayerMovement.cs
+++ ldjam50-lohja/Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+
     private Rigidbody2D myRigidBody;
 
     public void Start()
@@ -12,8 +14,9 @@ public class PlayerMovement : MonoBehaviour
         myRigidBody = this.GetComponent<Rigidbody2D>();
     }
 
-    public void MovePlayer(Vector2 movementDirection)
+    public void MovePlayer(InputAction.CallbackContext context)
     {
+        Vector2 movementDirection = context.ReadValue<Vector2>();
         myRigidBody.velocity = speed * movementDirection;
     }
 }
