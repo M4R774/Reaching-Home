35a9585ef6e3453f4c1ca5596dccc94abf5cccf9
Timo Sissala <timo.sissala@hotmail.com>
2022-04-02 14:22:17 +0300

    Saving fire positions

--- ldjam50-lohja/Assets/Scripts/Fire.cs
+++ ldjam50-lohja/Assets/Scripts/Fire.cs
@@ -11,6 +11,7 @@ public class Fire
 
     public Fire(Fire[,] fires, Vector3Int position)
     {
+        this.fires = fires;
         this.position = position;
     }
 
@@ -19,13 +20,13 @@ public class Fire
         neighbours = new List<Fire>();
         for (int y = position.y - 1; y <= position.y + 1; y++)
         {
-            for (int x = position.x; x <= position.x + 1; x++)
+            for (int x = position.x - 1; x <= position.x + 1; x++)
             {
                 if (IsInBounds(x, y))
                 {
                     Fire fire = fires[y, x];
 
-                    if (fire != null)
+                    if (fire != null && (y != position.y || x != position.x))
                     {
                         neighbours.Add(fire);
                     }
@@ -37,10 +38,15 @@ public class Fire
     public void PrintNeighbours()
     {
         Debug.Log(neighbours.Count);
+
+        if (neighbours.Count == 0)
+        {
+            Debug.Log(position);
+        }
     }
 
     private bool IsInBounds(int x, int y)
     {
-        return x < fires.GetLength(1) && x > 0 && y < fires.GetLength(0) && y > 0;
+        return x < fires.GetLength(1) && x >= 0 && y < fires.GetLength(0) && y >= 0;
     }
 }
