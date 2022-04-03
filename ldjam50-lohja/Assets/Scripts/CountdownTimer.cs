50e96f3709b2656bf15b6f79209fd5669fdfd462
Aleksi Martikainen <aleksi.martikainen@devecto.com>
2022-04-03 12:33:39 +0300

    Add countdown

--- ldjam50-lohja/Assets/Scripts/CountdownTimer.cs
+++ ldjam50-lohja/Assets/Scripts/CountdownTimer.cs
@@ -5,18 +5,21 @@ using UnityEngine.UI;
 
 public class CountdownTimer : MonoBehaviour
 {
+    public float remainingTime = 642f;
     public Text countdownText;
 
     // Update is called once per frame
     void Update()
     {
-        // DisplayTime(timeToDisplay)
+        remainingTime -= Time.deltaTime;
+        DisplayTime(remainingTime);
     }
 
     void DisplayTime(float timeToDisplay)
     {
         int minutes = Mathf.FloorToInt(timeToDisplay / 60);
         int seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        int milliseconds = Mathf.FloorToInt(timeToDisplay % 1f * 1000);
+        int milliseconds = Mathf.FloorToInt(timeToDisplay % 1f * 100);
+        countdownText.text = $"{minutes:00}:{seconds:00}:{milliseconds:00}";
     }
 }
