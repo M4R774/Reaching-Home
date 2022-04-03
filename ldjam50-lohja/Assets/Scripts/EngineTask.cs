a409d0246070fd066e041ac3cad701c07fe7abe6
KlausTele <55992403+KlausTele@users.noreply.github.com>
2022-04-03 11:23:48 +0300

    Engines with animations and interactions

--- ldjam50-lohja/Assets/Scripts/EngineTask.cs
+++ ldjam50-lohja/Assets/Scripts/EngineTask.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ComputerTask : Task
+public class EngineTask : Task
 {
     private void Break() 
     {
